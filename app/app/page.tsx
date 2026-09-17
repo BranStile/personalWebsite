c279026bf025f03ba749937577112a1fd6676118
Brandon Stile <bgstile06@gmail.com>
2026-09-17 09:39:50 -0400

    small spelling errors

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
   return (
   <div>
     <AboutMe/>
-    <h2 className="text-center text-3xl text-navigation-hover-text font-bold">Experiences</h2>
+    <h2 className="text-center text-3xl text-navigation-hover-text font-bold">Experience</h2>
     <hr className="w-3/4 rounded-2xl border-2 m-auto mt-6 mb-4 border-navigation-hover-text"></hr>
     <Experience initialExperience={experience}/>
   </div>
