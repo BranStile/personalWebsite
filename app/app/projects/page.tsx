b7cffbf6b24772cf1367df8c3a8af2c9c82fe04a
branStile <bgstile06@gmail.com>
2026-09-20 17:22:17 -0400

    added a projects component

--- app/app/projects/page.tsx
+++ app/app/projects/page.tsx
@@ -1,5 +1,15 @@
-import InfoCard from "../components/InfoCard";
+import { getProjects } from "@/prisma/prismaResults";
+import ProjectsExtended from "../components/ProjectsExtended";
 
-export default function Projects(){
-    return(<InfoCard></InfoCard>);
+export const dynamic = "force-dynamic";
+
+export default async function Projects(){
+    const projects = (await getProjects()).map((item) => ({
+        ...item,
+        startDate: item.startDate?.toISOString() ?? null,
+        endDate: item.endDate?.toISOString() ?? null,
+        createdAt: item.createdAt.toISOString(),
+    }));
+
+    return(<div><ProjectsExtended initialProjects = {projects}/></div>);
 }
